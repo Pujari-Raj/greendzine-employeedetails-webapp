9576102e6c63e358ac401c4c017a565d7708c000
Pujari Basvaraj <basvarajpujari607@gmail.com>
2023-12-07 20:09:27 +0530

    added tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,12 @@ import './App.css';
 function App() {
   return (
     <>
-  
+      <p className="text-xl">
+        Click on the Vite and React logos to learn more
+      </p>
+      <p className=''>
+      Employee Productivity Dashboard
+      </p>
     </>
   )
 }
