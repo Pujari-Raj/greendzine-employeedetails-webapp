4ca523d276c987742d767db8b0a8117ce21e4734
Pujari Basvaraj <basvarajpujari607@gmail.com>
2023-12-09 22:55:02 +0530

    completed dashboard page

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ProgressBar from "./ProgressBar";
 import { NavLink } from "react-router-dom";
 import { Home, User } from "lucide-react";
 
@@ -32,49 +31,44 @@ const Dashboard = () => {
           <div className="dashboard mb-[2rem] opacity-[0.8] p-[2rem]  rounded-bl-2xl rounded-br-2xl">
             {/* weekcards */}
             <div className="flex flex-col">
-              <div className="p-[0.5rem]">
+              <div className="mb-[1rem] p-[0.5rem]">
                 <div className="flex justify-between">
                   <p className="">Productivity on Monday</p>
                   <p className="text-[#36A546] ">4%</p>
                 </div>
-                <div className="text-left">
-                  <ProgressBar productivityvalue="10" />
+                <div className="progrees-bar-1">
                 </div>
               </div>
-              <div className="flex flex-col p-[0.5rem]">
+              <div className="mb-[1rem] flex flex-col p-[0.5rem] relative">
                 <div className="flex justify-between">
                   <p className="">Productivity on Tueday</p>
                   <p className="text-[#36A546] ">92%</p>
                 </div>
-                <div className="text-left">
-                  <ProgressBar productivityvalue="92" />
+                <div className="progrees-bar-2">
                 </div>
               </div>
-              <div className="flex flex-col p-[0.5rem]">
+              <div className="mb-[1rem] flex flex-col p-[0.5rem]">
                 <div className="flex justify-between">
                   <p className="">Productivity on Wednesday</p>
                   <p className="text-[#36A546] ">122%</p>
                 </div>
-                <div className="text-left">
-                  <ProgressBar productivityvalue="122" />
+                <div className="progrees-bar-3">
                 </div>
               </div>
-              <div className="flex flex-col p-[0.5rem]">
+              <div className="mb-[1rem] flex flex-col p-[0.5rem]">
                 <div className="flex justify-between">
                   <p className="">Productivity on Thursday</p>
                   <p className="text-[#36A546] ">93%</p>
                 </div>
-                <div className="text-left">
-                  <ProgressBar productivityvalue="93" />
+                <div className="progrees-bar-4">
                 </div>
               </div>
-              <div className="flex flex-col p-[0.5rem]">
+              <div className="mb-[1rem] flex flex-col p-[0.5rem]">
                 <div className="flex justify-between">
                   <p className="">Productivity on Friday</p>
                   <p className="text-[#36A546] ">89%</p>
                 </div>
-                <div className="text-left">
-                  <ProgressBar productivityvalue="89" />
+                <div className="progrees-bar-5">
                 </div>
               </div>
               <div className="flex flex-col p-[0.5rem]">
@@ -82,52 +76,15 @@ const Dashboard = () => {
                   <p className="">Productivity on Saturday</p>
                   <p className="text-[#36A546] ">98%</p>
                 </div>
-                <div className="text-left">
-                  <ProgressBar productivityvalue="98" />
+                <div className="progrees-bar-6">
                 </div>
               </div>
-
-              {/* <div className="grid grid-cols-2 p-[0.5rem]">
-                <div className="text-left">
-                  <p className="text-left">Productivity on Tueday</p>
-                  <ProgressBar productivityvalue="92" />
-                </div>
-                <p className="text-[#36A546] text-right">92%</p>
-              </div>
-              <div className="grid grid-cols-2 p-[0.5rem]">
-                <div className="text-left">
-                  <p className="text-left">Productivity on Wednesday</p>
-                  <ProgressBar productivityvalue="122" />
-                </div>
-                <p className="text-[#36A546] text-right">122%</p>
-              </div>
-              <div className="grid grid-cols-2 p-[0.5rem]">
-                <div className="text-left">
-                  <p className="text-left">Productivity on Thursday</p>
-                  <ProgressBar productivityvalue="93" />
-                </div>
-                <p className="text-[#36A546] text-right">93%</p>
-              </div>
-              <div className="grid grid-cols-2 p-[0.5rem]">
-                <div className="text-left">
-                  <p className="text-left">Productivity on Friday</p>
-                  <ProgressBar productivityvalue="89" />
-                </div>
-                <p className="text-[#36A546] text-right">89%</p>
-              </div>
-              <div className="grid grid-cols-2 p-[0.5rem]">
-                <div className="text-left">
-                  <p className="text-left">Productivity on Saturday</p>
-                  <ProgressBar productivityvalue="98" />
-                </div>
-                <p className="text-[#36A546] text-right">98%</p>
-              </div> */}
             </div>
           </div>
         </div>
       </div>
       <footer
-        className="fixed bottom-0 left-0 z-20 w-full border-[#36A546] rounded-t-[2.5rem] rounded-lg bg-[#5E5E5E82] md:hidden flex  "
+        className="fixed bottom-0 left-0 z-20 w-full border-[#36A546] rounded-t-[2.5rem] rounded-lg bg-[#5E5E5E82] md:hidden flex "
         id="mobileNav"
       >
         <div className="flex justify-center items-center">
